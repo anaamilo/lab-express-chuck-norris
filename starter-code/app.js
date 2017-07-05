4f8a3347386b1801e67975c444c98e677416257e
Ana Milojevic <anamilojevicd@gmail.com>
2017-07-05 16:10:36 +0200

    wip

--- starter-code/app.js
+++ starter-code/app.js
@@ -3,3 +3,18 @@ const app = express();
 const Chuck  = require('chucknorris-io');
 const client = new Chuck();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/random', (req, res) => {
+  client.getRandomJoke()
+    .then((response) => {
+      console.log(response);
+    }).catch((err) => {
+      console.log(err);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("App is running!");
+})
